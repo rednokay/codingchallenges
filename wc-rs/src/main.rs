8d344d401e49df74a86a4ddcb3c6de0f503c1357
rednokay <207755534+rednokay@users.noreply.github.com>
2025-12-18 12:51:07 +0100

    Add byte counter

--- wc-rs/src/main.rs
+++ wc-rs/src/main.rs
@@ -1,3 +1,42 @@
+use clap::Parser;
+use std::fs;
+
+fn read_file(path: &String) -> String {
+    return fs::read_to_string(path).expect("File not found");
+}
+
+fn count_bytes(text: &String) -> usize {
+    return text.len();
+}
+
+#[derive(Debug, Parser)]
+struct Args {
+    #[arg(short = 'c', long)]
+    bytes: bool,
+
+    #[arg(short, long)]
+    lines: bool,
+
+    path: Option<String>,
+}
+
+fn cli(args: Args) {
+    if args.bytes {
+        if let Some(path) = args.path {
+            let contents = read_file(&path);
+            let count = count_bytes(&contents);
+            println!("{} {}", count, path);
+        }
+    }
+    if args.lines {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    if args.path == None {
+        println!("No path given");
+    } else {
+        cli(args);
+    }
 }
